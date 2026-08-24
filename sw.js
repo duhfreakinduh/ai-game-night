760547f7e2c0ea7e48023011ec131654f807ca07
duhfreakinduh <duhfreakinduh@gmail.com>
2026-08-23 22:35:34 -0500

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='brain-bash-v1';
+const CORE=['./','./index.html','./styles.css','./app.js','./questions.js','./ai.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
